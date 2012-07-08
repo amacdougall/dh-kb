eec14372826b1a0a676f9db2e355322a227a0955
amacdougall <smoke@alanmacdougall.com>
2012-07-07 23:26:06 -0400

    Move city/region grouping logic from controller to model.

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,4 +3,25 @@ class City < ActiveRecord::Base
 
   belongs_to :region
   has_many :characters
+
+  def self.grouped_by_region
+    # TODO: revise to use a join or a scope or something
+    regions = Region.order "name"
+    cities = City.order "name"
+    cities_by_region = []
+
+    regions.each do |region|
+      cities_for_region = cities.select {|city| city.region_id == region.id}
+      unless cities_for_region.empty?
+        cities_by_region << {name: region.name, cities: cities_for_region}
+      end
+    end
+
+    cities_by_region << {
+      name: "Uncategorized",
+      cities: cities.select {|city| city.region_id.nil?}
+    }
+
+    cities_by_region
+  end
 end
